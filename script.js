fd29fe795af9668bfbe2e1e52ba3a1e6a7e4d1ed
annaegorovaa <anju.egorova@gmail.com>
2018-07-25 13:15:26 -0700

    number and operand buttons working

--- script.js
+++ script.js
@@ -1,14 +1,70 @@
-function toInput(id) {
-    let value = document.getElementById(id).value;
-    document.getElementById('result').value += value;
+let a = 0;
+let b = 0;
+let operand = '';
+
+function read() {
+    return +document.getElementById('result').value || 0;
+}
+
+function write(value) {
+    document.getElementById('result').value = value;
+}
+
+function toInput(value) {
+    if (operand) {
+        if (b) {
+            b = read() + value;
+            write(b);
+        } else {
+            write(value);
+            b = value;
+        }
+    } else {
+        write(read() + value);
+    }
 }
 
 function subtract(){
-    const a = document.getElementById('').value;
-    const b = document.getElementById('').value;
-    return a-b;
+    a = read();
+    operand = '-';
+}
+
+function addNum() {
+    a = read();
+    operand = '+';
+}
+
+function multiply() {
+    a = read();
+    operand = '*';
+}
+
+function divide() {
+    a = read();
+    operand = '/';
+}
+
+function getResult() {
+    if (operand) {
+        switch (operand) {
+            case '-':
+                write(a - b);
+                break;
+            case '+':
+                write(a + +b);
+                break;
+            case '*':
+                write(a * b);
+                break;
+            case '/':
+                write(a / b);
+                break;
+        }
+    }
 }
 
 function clearValue() {
-    document.getElementById('result').value = '';
+    document.getElementById('result').value = '0';
+    a = b = 0;
+    operand = '';
 }
