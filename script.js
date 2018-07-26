5c0dc717ab34c5e895fcff3420a75aa335b9530b
annaegorovaa <anju.egorova@gmail.com>
2018-07-26 11:05:02 -0700

    eslint corrections

--- script.js
+++ script.js
@@ -3,68 +3,68 @@ let b = 0;
 let operation = '';
 
 function read() {
-    return +document.getElementById('result').value || 0;
+  return document.getElementById('result').value || '0';
 }
 
 function write(value) {
-    document.getElementById('result').value = value;
+  document.getElementById('result').value = value;
 }
 
 function toInput(value) {
-    if (operation) {
-        if (b) {
-            b = read() + value;
-            write(b);
-        } else {
-            write(value);
-            b = value;
-        }
+  if (operation) {
+    if (b) {
+      b = read() + value;
+      write(b);
     } else {
-        write(read() + value);
+      write(value);
+      b = value;
     }
+  } else {
+    write(read() + value);
+  }
 }
 
 function subtract(){
-    a = read();
-    operation = '-';
+  a = read();
+  operation = '-';
 }
 
 function addNum() {
-    a = read();
-    operation = '+';
+  a = read();
+  operation = '+';
 }
 
 function multiply() {
-    a = read();
-    operation = '*';
+  a = read();
+  operation = '*';
 }
 
 function divide() {
-    a = read();
-    operation = '/';
+  a = read();
+  operation = '/';
 }
 
 function getResult() {
-    if (operation) {
-        switch (operation) {
-            case '-':
-                write(a - b);
-                break;
-            case '+':
-                write(a + +b);
-                break;
-            case '*':
-                write(a * b);
-                break;
-            case '/':
-                write(a / b);
-                break;
-        }
+  if (operation) {
+    switch (operation) {
+      case '-':
+        write(a - b);
+        break;
+      case '+':
+        write(a + +b);
+        break;
+      case '*':
+        write(a * b);
+        break;
+      case '/':
+        write(a / b);
+        break;
     }
+  }
 }
 
 function clearValue() {
-    document.getElementById('result').value = '0';
-    a = b = 0;
-    operation = '';
+  document.getElementById('result').value = '0';
+  a = b = 0;
+  operation = '';
 }
