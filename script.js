10ce3b42c25e3f0b33a862fd729813dbd09ec255
JetFighterrr <soigoshin@gmail.com>
2018-07-26 18:17:48 -0700

    Add my logic (#24)
    * some logic added
    
    * buttons moved
    
    * change lane endings

--- script.js
+++ script.js
@@ -1,14 +1,78 @@
+let a=0,
+    initialState = true,
+    operand = null;
+
+document.getElementById('result').value = '0';
+
 function toInput(id) {
-    let value = document.getElementById(id).value;
-    document.getElementById('result').value += value;
+    console.log(operand);
+    console.log(initialState);
+    let temp = document.getElementById('result').value;
+    if (initialState) {
+        clearValue();
+        initialState = false;
+    }
+    if (temp === '0') document.getElementById('result').value = id;
+    else document.getElementById('result').value += id;
 }
 
-function subtract(){
-    const a = document.getElementById('').value;
-    const b = document.getElementById('').value;
-    return a-b;
+function toInputClean(id){
+    clearValue();
+    toInput(id);
+}
+
+function fromInput(){
+    return document.getElementById('result').value;
+}
+
+function fromInputClean(){
+    let result = document.getElementById('result').value;
+    clearValue();
+    return result;
+}
+
+function initAction(id){
+    initialState = true;
+    if (operand === null){
+        a = Number(fromInput());
+        operand = id;
+    }
+    else{
+        operand = id;
+        switchOperand();
+        toInput(a.toString());
+        initialState = true;
+    }
+}
+
+function switchOperand(){
+    switch(operand){
+        case '-': a -= Number(fromInputClean());
+        break;
+        case '+': a += Number(fromInputClean());
+        break;
+        case '*': a *= Number(fromInputClean());
+        break;
+        case '/': a = a / Number(fromInputClean());
+        break;
+    }
+}
+
+function getResult(){
+    switchOperand();
+    toInputClean(a.toString());
+    a = 0;
+    initialState = true;
+    operand = null;
 }
 
 function clearValue() {
     document.getElementById('result').value = '';
+}
+
+function clearToInitialState() {
+    clearValue();
+    a = 0;
+    initialState = true;
+    operand = null;
 }
