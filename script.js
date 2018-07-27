b8a702372a1261009151e480693075e0cd142894
Irinabez <irinabez@gmail.com>
2018-07-27 14:48:55 -0700

    Divide button (#27)
    * Added divide button to updated index
    
    * Fixed divide button to work with Sergey's code
    
    * Fixed divide button to work with Sergey's code. Fixed ESLINT code formatting in script.js file
    
    * Fixed divide button to work with Sergey's code. Fixed ESLINT code formatting in script.js file
    
    * ESLINT format js file plus fixed Div button to work with current function
    
    * ESLINT format js file plus fixed Div button to work with current function

--- script.js
+++ script.js
@@ -1,78 +1,78 @@
 let a=0,
-    initialState = true,
-    operand = null;
+  initialState = true,
+  operand = null;
 
 document.getElementById('result').value = '0';
 
 function toInput(id) {
-    console.log(operand);
-    console.log(initialState);
-    let temp = document.getElementById('result').value;
-    if (initialState) {
-        clearValue();
-        initialState = false;
-    }
-    if (temp === '0') document.getElementById('result').value = id;
-    else document.getElementById('result').value += id;
+  console.log(operand);
+  console.log(initialState);
+  let temp = document.getElementById('result').value;
+  if (initialState) {
+    clearValue();
+    initialState = false;
+  }
+  if (temp === '0') document.getElementById('result').value = id;
+  else document.getElementById('result').value += id;
 }
 
 function toInputClean(id){
-    clearValue();
-    toInput(id);
+  clearValue();
+  toInput(id);
 }
 
 function fromInput(){
-    return document.getElementById('result').value;
+  return document.getElementById('result').value;
 }
 
 function fromInputClean(){
-    let result = document.getElementById('result').value;
-    clearValue();
-    return result;
+  let result = document.getElementById('result').value;
+  clearValue();
+  return result;
 }
 
 function initAction(id){
+  initialState = true;
+  if (operand === null){
+    a = Number(fromInput());
+    operand = id;
+  }
+  else{
+    operand = id;
+    switchOperand();
+    toInput(a.toString());
     initialState = true;
-    if (operand === null){
-        a = Number(fromInput());
-        operand = id;
-    }
-    else{
-        operand = id;
-        switchOperand();
-        toInput(a.toString());
-        initialState = true;
-    }
+  }
 }
 
 function switchOperand(){
-    switch(operand){
-        case '-': a -= Number(fromInputClean());
-        break;
-        case '+': a += Number(fromInputClean());
-        break;
-        case '*': a *= Number(fromInputClean());
-        break;
-        case '/': a = a / Number(fromInputClean());
-        break;
-    }
+  switch(operand){
+    case '-': a -= Number(fromInputClean());
+      break;
+    case '+': a += Number(fromInputClean());
+      break;
+    case '*': a *= Number(fromInputClean());
+      break;
+    case '/': a = a / Number(fromInputClean());
+      break;
+  }
 }
 
 function getResult(){
-    switchOperand();
-    toInputClean(a.toString());
-    a = 0;
-    initialState = true;
-    operand = null;
+  switchOperand();
+  toInputClean(a.toString());
+  a = 0;
+  initialState = true;
+  operand = null;
 }
 
 function clearValue() {
-    document.getElementById('result').value = '';
+  document.getElementById('result').value = '';
 }
 
 function clearToInitialState() {
-    clearValue();
-    a = 0;
-    initialState = true;
-    operand = null;
+  clearValue();
+  a = 0;
+  initialState = true;
+  operand = null;
 }
