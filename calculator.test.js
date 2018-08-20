dd6b76bf95978fa38b5c007e2887a3cf24ef16e8
annaegorovaa <anju.egorova@gmail.com>
2018-08-20 12:51:47 -0700

    correct test function handleKey

--- calculator.test.js
+++ calculator.test.js
@@ -1,44 +1,50 @@
 const Calculator = require('./src/calculator');
 
-const calc = new Calculator(() => {}, () => {});
+let calc = null;
 
-test('handleKey should handle numbers', () => {
-  expect(calc.handleKey('0')).toBe(true);
-  expect(calc.handleKey('1')).toBe(true);
-  expect(calc.handleKey('2')).toBe(true);
-  expect(calc.handleKey('3')).toBe(true);
-  expect(calc.handleKey('4')).toBe(true);
-  expect(calc.handleKey('5')).toBe(true);
-  expect(calc.handleKey('6')).toBe(true);
-  expect(calc.handleKey('7')).toBe(true);
-  expect(calc.handleKey('8')).toBe(true);
-  expect(calc.handleKey('9')).toBe(true);
+beforeEach(() => {
+  calc = new Calculator(() => {}, () => {});
 });
 
-test('handleKey should handle dot', () => {
-  expect(calc.handleKey('.')).toBe(true);
-});
+describe('test handleKey function', () => {
+  test('handleKey should handle numbers', () => {
+    expect(calc.handleKey('0')).toBe(true);
+    expect(calc.handleKey('1')).toBe(true);
+    expect(calc.handleKey('2')).toBe(true);
+    expect(calc.handleKey('3')).toBe(true);
+    expect(calc.handleKey('4')).toBe(true);
+    expect(calc.handleKey('5')).toBe(true);
+    expect(calc.handleKey('6')).toBe(true);
+    expect(calc.handleKey('7')).toBe(true);
+    expect(calc.handleKey('8')).toBe(true);
+    expect(calc.handleKey('9')).toBe(true);
+  });
 
-test('handleKey should handle operation signs', () => {
-  expect(calc.handleKey('+')).toBe(true);
-  expect(calc.handleKey('-')).toBe(true);
-  expect(calc.handleKey('*')).toBe(true);
-  expect(calc.handleKey('/')).toBe(true);
-});
+  test('handleKey should handle dot', () => {
+    expect(calc.handleKey('.')).toBe(true);
+  });
 
-test('handleKey should handle percent sign', () => {
-  expect(calc.handleKey('%')).toBe(true);
-});
+  test('handleKey should handle operation signs', () => {
+    expect(calc.handleKey('+')).toBe(true);
+    expect(calc.handleKey('-')).toBe(true);
+    expect(calc.handleKey('*')).toBe(true);
+    expect(calc.handleKey('/')).toBe(true);
+  });
 
-test('handleKey should handle enter', () => {
-  expect(calc.handleKey('Enter')).toBe(true);
-});
+  test('handleKey should handle percent sign', () => {
+    expect(calc.handleKey('%')).toBe(true);
+  });
 
-test('handleKey should not handle unknown key', () => {
-  expect(calc.handleKey('r')).toBe(false);
-  expect(calc.handleKey('?')).toBe(false);
-  expect(calc.handleKey('$')).toBe(false);
-  expect(calc.handleKey('&')).toBe(false);
-  expect(calc.handleKey('\\')).toBe(false);
-  expect(calc.handleKey('M')).toBe(false);
+  test('handleKey should handle enter', () => {
+    expect(calc.handleKey('Enter')).toBe(true);
+  });
+
+  test('handleKey should not handle unknown key', () => {
+    expect(calc.handleKey('r')).toBe(false);
+    expect(calc.handleKey('?')).toBe(false);
+    expect(calc.handleKey('$')).toBe(false);
+    expect(calc.handleKey('&')).toBe(false);
+    expect(calc.handleKey('\\')).toBe(false);
+    expect(calc.handleKey('M')).toBe(false);
+  });
 });
