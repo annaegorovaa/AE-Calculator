2393eb47b99309e525188fe1ac6a913077f4ec4f
annaegorovaa <anju.egorova@gmail.com>
2018-08-20 19:37:24 -0700

    test function addSymbol

--- calculator.test.js
+++ calculator.test.js
@@ -1,9 +1,13 @@
 const Calculator = require('./src/calculator');
 
 let calc = null;
+let output = '';
 
 beforeEach(() => {
-  calc = new Calculator(() => {}, () => {});
+  const writeCallback = (val) => {
+    output = val;
+  };
+  calc = new Calculator(writeCallback, () => {});
 });
 
 describe('test handleKey function', () => {
@@ -46,5 +50,76 @@ describe('test handleKey function', () => {
     expect(calc.handleKey('&')).toBe(false);
     expect(calc.handleKey('\\')).toBe(false);
     expect(calc.handleKey('M')).toBe(false);
+  });
+});
+
+describe('test addSymbol function', () => {
+  beforeEach(() => {
+    output = '';
+  });
+
+  describe('test addSymbol if first digit', () => {
+    test('if number should return number', () => {
+      calc.addSymbol('1');
+      expect(output).toBe('1');
+    });
+
+    test('if minus should return change sign', () => {
+      calc.addSymbol('-');
+      expect(output).toBe('-');
+    });
+
+    test('if dot should return zero and dot', () => {
+      calc.addSymbol('.');
+      expect(output).toBe('0.');
+    });
+  });
+
+  describe('test addSymbol if has digits without dots', () => {
+    test('if number should add number', () => {
+      calc.addSymbol('1');
+      calc.addSymbol('3');
+      expect(output).toBe('13');
+    });
+
+    test('if dot should add dot', () => {
+      calc.addSymbol('1');
+      calc.addSymbol('.');
+      expect(output).toBe('1.');
+    });
+
+    test('if minus should add minus before number', () => {
+      calc.addSymbol('1');
+      calc.addSymbol('-');
+      expect(output).toBe('-1');
+    });
+
+    test('if first digit is zero should not add zero', () => {
+      calc.addSymbol('0');
+      calc.addSymbol('0');
+      expect(output).toBe('0');
+    });
+
+    test('if first digit is zero should replace zero with number', () => {
+      calc.addSymbol('0');
+      calc.addSymbol('1');
+      expect(output).toBe('1');
+    });
+  });
+
+  describe('test addSymbol if has digits with dots', () => {
+    test('if number should add number', () => {
+      calc.addSymbol('1');
+      calc.addSymbol('.');
+      calc.addSymbol('0');
+      expect(output).toBe('1.0');
+    });
+
+    test('if dot should not add dot', () => {
+      calc.addSymbol('1');
+      calc.addSymbol('.');
+      calc.addSymbol('.');
+      expect(output).toBe('1.');
+    });
   });
 });
